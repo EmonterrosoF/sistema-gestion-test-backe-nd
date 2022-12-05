f0743f7487b57ee3b2fe22a188b13c32a35d1913
EmonterrosoF <monterrosoedras.@gmail.com>
2022-12-04 22:35:19 -0600

    first commit

---
+++ src/main/java/com/sistema/examenes/SistemaExamenesBackendApplication.java
@@ -0,0 +1,12 @@
+package com.sistema.examenes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaExamenesBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaExamenesBackendApplication.class, args);
+	}
+}
